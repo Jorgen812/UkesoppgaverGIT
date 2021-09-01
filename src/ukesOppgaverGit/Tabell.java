1d1df9142b7d3581bbc42d4193c70456c2a0b714
Jorgen812 <traasdahl@gmail.com>
2021-09-01 11:44:26 +0200

    Ny commit

--- src/ukesOppgaverGit/Tabell.java
+++ src/ukesOppgaverGit/Tabell.java
@@ -1,5 +1,6 @@
 package ukesOppgaverGit;
 
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 public class Tabell {
@@ -11,6 +12,83 @@ public class Tabell {
     {
         int temp = a[i]; a[i] = a[j]; a[j] = temp;
     }
+
+    public static void bytt(char[] c, int i, int j){
+
+        char temp = c[i]; c[i] = c[j]; c[j] = temp;
+    }
+
+
+
+    /*
+    Lag metoden public static void skriv(int[] a, int fra, int til). Den skal skrive ut tallene i intervallet
+    a[fra:til> til konsollet - alle på én linje og et mellomrom mellom hvert tall.
+     */
+
+    public static void skriv(int [] a, int fra, int til){
+
+        for (int i = fra; i < til; i++){
+            System.out.print(a[i]+" ");
+        }
+
+    }
+
+    public static void skriv(int[] a) {
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    public static void skrivln(int [] a, int fra, int til){
+
+        fratilKontroll(a.length,fra,til);
+
+        for (int i = fra; i < til; i++){
+            System.out.println(a[i]+" ");
+        }
+
+    }
+
+    public static void skrivln(int [] a){
+
+        for (int i = 0; i < a.length; i++){
+            System.out.println(a[i]+" ");
+        }
+    }
+
+    public static void fratilKontroll(int tablengde, int fra, int til)
+    {
+        if (fra < 0)                                  // fra er negativ
+            throw new ArrayIndexOutOfBoundsException
+                    ("fra(" + fra + ") er negativ!");
+
+        if (til > tablengde)                          // til er utenfor tabellen
+            throw new ArrayIndexOutOfBoundsException
+                    ("til(" + til + ") > tablengde(" + tablengde + ")");
+
+        if (fra > til)                                // fra er større enn til
+            throw new IllegalArgumentException
+                    ("fra(" + fra + ") > til(" + til + ") - illegalt intervall!");
+    }
+
+    public static void vhKontroll(int tablengde, int v, int h)
+    {
+        if (v < 0)
+            throw new ArrayIndexOutOfBoundsException("v(" + v + ") < 0");
+
+        if (h >= tablengde)
+            throw new ArrayIndexOutOfBoundsException
+                    ("h(" + h + ") >= tablengde(" + tablengde + ")");
+
+        if (v > h + 1)
+            throw new IllegalArgumentException
+                    ("v = " + v + ", h = " + h);
+    }
+
+
+
+
 
     public static int[] randPerm(int n)
     {
@@ -42,8 +120,14 @@ public class Tabell {
 
     public static int maks(int[] a, int fra, int til)
     {
-        if (fra < 0 || til > a.length || fra >= til)
-            throw new IllegalArgumentException("Illegalt intervall!");
+        fratilKontroll(a.length,fra,til);
+
+        if (a.length == 0)
+            throw new NoSuchElementException("Tabellen er tom");
+        if (fra == til)
+            throw new NoSuchElementException
+                    ("fra(" + fra + ") = til(" + til + ") - tomt tabellintervall!");
+
 
         int m = fra;             // indeks til største verdi i a[fra:til>
         int maksverdi = a[fra];  // største verdi i a[fra:til>
@@ -83,5 +167,67 @@ public class Tabell {
     {
         return min(a,0,a.length);  // kaller metoden over
     }
+
+    public static int[] nestMaks(int[] a)  // legges i class Tabell
+    {
+        int n = a.length;   // tabellens lengde
+
+        if (n < 2) throw   // må ha minst to verdier!
+                new java.util.NoSuchElementException("a.length(" + n + ") < 2!");
+
+        int m = maks(a);  // m er posisjonen til tabellens største verdi
+
+        int nm;           // nm skal inneholde posisjonen til nest største verdi
+
+        if (m == 0)                            // den største ligger først
+        {
+            nm = maks(a, 1, n);                  // leter i a[1:n>
+        }
+        else if (m == n - 1)                   // den største ligger bakerst
+        {
+            nm = maks(a, 0, n - 1);              // leter i a[0:n-1>
+        }
+        else
+        {
+            int mv = maks(a, 0, m);              // leter i a[0:m>
+            int mh = maks(a, m + 1, n);          // leter i a[m+1:n>
+            nm = a[mh] > a[mv] ? mh : mv;        // hvem er størst?
+        }
+
+        return new int[] {m,nm};      // m i posisjon 0 , nm i posisjon 1
+
+    } // nestMaks
+
+    public static int[] nestMaksBytt(int[] a)  // legges i class Tabell
+    {
+        int n = a.length;   // tabellens lengde
+
+        if (n < 2) throw   // må ha minst to verdier!
+                new java.util.NoSuchElementException("a.length(" + n + ") < 2!");
+
+        int m = maks(a);  // m er posisjonen til tabellens største verdi
+
+        int nm;           // nm skal inneholde posisjonen til nest største verdi
+
+
+        if (m == 0)                            // den største ligger først
+        {
+            nm = maks(a, 1, n);                  // leter i a[1:n>
+        }
+        else if (m == n - 1)                   // den største ligger bakerst
+        {
+            nm = maks(a, 0, n - 1);              // leter i a[0:n-1>
+        }
+        else
+        {
+            int mv = maks(a, 0, m);              // leter i a[0:m>
+            int mh = maks(a, m + 1, n);          // leter i a[m+1:n>
+            nm = a[mh] > a[mv] ? mh : mv;        // hvem er størst?
+        }
+
+        return new int[] {m,nm};      // m i posisjon 0 , nm i posisjon 1
+
+    } // nestMaks
+
 
 } // class Tabell
--- src/ukesOppgaverGit/UkesOppgaverGit.java
+++ src/ukesOppgaverGit/UkesOppgaverGit.java
@@ -6,7 +6,37 @@ import static ukesOppgaverGit.Tabell.*;
 public class UkesOppgaverGit {
     public static void main (String [] args){
 
-       int [] tabell = {1,4,3,12,5,10,20,34};
+
+        /*
+        int[] a = Tabell.randPerm(10);              // en tilfeldig tabell
+
+        for (int k : a) System.out.print(k + " ");  // skriver ut a
+
+        int m = Tabell.maks(a);   // finner posisjonen til største verdi
+
+        System.out.println("\nStørste verdi ligger på plass " + m);
+         */
+        int[] a = Tabell.randPerm(20); // tilfeldig permutasjon av 1 . . 20
+        int[] b = Tabell.nestMaks(a);  // metoden returnerer en tabell
+
+        int m = b[0], nm = b[1];       // m for maks, nm for nestmaks
+
+        Tabell.skrivln(a);        // se Oppgave 5 i Avsnitt 1.2.2
+        System.out.print("Størst(" + a[m] + ") har posisjon " + m);
+        System.out.println(", nest størst(" + a[nm] + ") har posisjon " + nm);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
