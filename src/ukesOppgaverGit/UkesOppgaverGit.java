58dc3a8ae3edf2ded6ef202864b0b8ddcd0b018a
Jorgen812 <traasdahl@gmail.com>
2021-09-01 09:32:41 +0200

    La til array i main og imporeterte tabell klasse til main.

--- src/ukesOppgaverGit/UkesOppgaverGit.java
+++ src/ukesOppgaverGit/UkesOppgaverGit.java
@@ -1,14 +1,12 @@
 package ukesOppgaverGit;
 
+
+import static ukesOppgaverGit.Tabell.*;
+
 public class UkesOppgaverGit {
     public static void main (String [] args){
 
-        int test = 0;
-
-
-
-
-
+       int [] tabell = {1,4,3,12,5,10,20,34};
 
 
 
